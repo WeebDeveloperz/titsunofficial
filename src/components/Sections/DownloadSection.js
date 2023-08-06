5b0a8b9bbd4d0acacd0652d56067b802ca678870
Rahul Goyal <rahulgoyalvinay666@gmail.com>
2023-08-06 15:53:43 +0530

    commitpush

--- src/components/Sections/DownloadSection.js
+++ src/components/Sections/DownloadSection.js
@@ -12,13 +12,13 @@ const DownloadSection = ({title,bgImage="", bgColor = "black", fgColor = "white"
         backgroundImage: bgImage
       }}
     >
-      {title &&
+      {/* {title &&
         <SectionTitle
           title={title}
           bgColor={bgColorTitle}
           fgColor={fgColorTitle} 
         />
-      }
+      } */}
       <div
         className={styles.options}
         style={{
@@ -26,9 +26,47 @@ const DownloadSection = ({title,bgImage="", bgColor = "black", fgColor = "white"
           backgroundColor: bgColorTitle
         }}
       > 
-       
+       <div className={styles.cse} >
+          CSE
+       </div>
+       <div className={styles.cse}>
+          ECE
+       </div>
       </div>
       
+      <div
+        className={styles.semesters}
+        style={{
+          color: fgColorTitle,
+          backgroundColor: bgColorTitle
+        }}
+      > 
+       <div className={styles.one} >
+          1
+       </div>
+       <div className={styles.one}>
+          2
+       </div>
+       <div className={styles.one}>
+          3
+       </div>
+       <div className={styles.one}>
+          4
+       </div>
+       <div className={styles.one}>
+          5
+       </div>
+       <div className={styles.one}>
+          6
+       </div>
+       <div className={styles.one}>
+          7
+       </div>
+       <div className={styles.one}>
+          8
+       </div>
+      </div>
+
       <div className={styles.flex}>
         {cards}
       </div>
